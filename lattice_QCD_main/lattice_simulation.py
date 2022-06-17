1d611ee9f62e810b5a6ae9d7094fec691ef24e6a
lucamarsili <105172952+lucamarsili@users.noreply.github.com>
2022-06-17 11:44:08 +0200

    Add files via upload

--- lattice_QCD_main/lattice_simulation.py
+++ lattice_QCD_main/lattice_simulation.py
@@ -14,7 +14,8 @@ import random_matrices_generator as g
 import numba
 
 ######define properties of lattice
-beta = 5.5
+beta = 1.719
+u0 = 0.797
 N = 4
 
 N_cor = 20
@@ -61,7 +62,58 @@ def plaquette(mu,nu, x,link):
     return np.absolute((1/3)*np.real(np.trace(staple)))
     #g.printing_matrix(np.dot(staple,np.transpose(np.conjugate(staple))))
 
-def wilson_loop_2aa(mu,nu, x,link):
+
+def plaquette_new(mu,nu, x,link):
+    staple = link[x[0]][x[1]][x[2]][x[3]][mu]
+    #g.printing_matrix(staple)
+    xpmu = moveup(x,mu)
+    xmmu = movedown(x,mu)
+    xpnu = moveup(x,nu)
+    xmnu = movedown(x,nu)
+    xpmunu = moveup(xpnu,mu)
+    staple = np.dot(staple,  link[xpmu[0]][xpmu[1]][xpmu[2]][xpmu[3]][nu])
+    staple = np.dot(staple,  np.conjugate(link[xpnu[0]][xpnu[1]][xpnu[2]][xpnu[3]][mu]))
+    #g.printing_matrix(staple)
+    staple = np.dot(staple, np.conjugate(link[x[0]][x[1]][x[2]][x[3]][nu]))
+    return np.absolute((1/3)*np.real(np.trace(staple)))
+    #g.printing_matrix(np.dot(staple,np.transpose(np.conjugate(staple))))
+
+def S_new(mu,x,link):  ###works better 0.48 and acceptance rate 0.45/0.50 don't touch
+    a = 0
+    for deltamu in range(1,mu+1):
+        a = a +  plaquette(mu,mu-deltamu,x,link) + plaquette(mu,mu-deltamu,movedown(x,mu-deltamu),link)
+    for deltamu in range(1,3-mu+1):
+        a = a + plaquette(mu+deltamu,mu,movedown(x,mu+deltamu),link)+ plaquette(mu+deltamu,mu,x,link)
+    return -beta*a
+def S_new_improved(mu,x,link):
+    a = 0
+    for deltamu in range(1,mu+1):
+        a = a+ wilson_loop_2aa_new(mu,mu-deltamu,x,link)+wilson_loop_a2a_new(mu,mu-deltamu,x,link)
+    for deltamu in range(1,3-mu+1):
+        a = a + wilson_loop_a2a_new(mu+deltamu,mu,movedown(x,mu+deltamu),link)+ wilson_loop_2aa_new(mu+deltamu,mu,movedown(movedown(x,mu+deltamu),mu+deltamu),link)
+    return S_new(mu,x,link)*(1/u0**4) + beta*a*(1/u0**6)*(1/12)
+      
+    
+def wilson_loop_a2a_new(mu,nu, x,link):
+    staple = link[x[0]][x[1]][x[2]][x[3]][mu]
+    #g.printing_matrix(staple)
+    xpmu = moveup(x,mu)
+    xmmu = movedown(x,mu)
+    xpnu = moveup(x,nu)
+    xpmunu = moveup(xpnu,mu)
+    xppnu = moveup(moveup(x,nu),nu)
+    xmnu = movedown(x,nu)
+    xppmunu = moveup(moveup(moveup(xpnu,mu),mu),nu)
+    staple = np.dot(staple,  link[xpmu[0]][xpmu[1]][xpmu[2]][xpmu[3]][nu])
+    staple = np.dot(staple,  link[xpmunu[0]][xpmunu[1]][xpmunu[2]][xpmunu[3]][nu])
+    staple = np.dot(staple,  np.conjugate(link[xppnu[0]][xppnu[1]][xppnu[2]][xppnu[3]][mu]))
+    staple = np.dot(staple,  np.conjugate(link[xpnu[0]][xpnu[1]][xpnu[2]][xpnu[3]][nu]))
+    #g.printing_matrix(staple)
+    staple = np.dot(staple, np.conjugate(link[x[0]][x[1]][x[2]][x[3]][nu]))
+    return np.absolute((1/3)*np.real(np.trace(staple)))
+    #g.printing_matrix(np.dot(staple,np.transpose(np.conjugate(staple))))
+
+def wilson_loop_2aa_new(mu,nu,x,link):
     staple = link[x[0]][x[1]][x[2]][x[3]][mu]
     #g.printing_matrix(staple)
     xpmu = moveup(x,mu)
@@ -71,32 +123,16 @@ def wilson_loop_2aa(mu,nu, x,link):
     xppmu = moveup(moveup(x,mu),mu)
     xmnu = movedown(x,nu)
     xppmunu = moveup(moveup(moveup(xpnu,mu),mu),nu)
-    staple = np.dot(staple,  link[xpmu[0]][xpmu[1]][xpmu[2]][xpmu[3]][nu])
+    staple = np.dot(staple,  link[xpmu[0]][xpmu[1]][xpmu[2]][xpmu[3]][mu])
     staple = np.dot(staple,  link[xppmu[0]][xppmu[1]][xppmu[2]][xppmu[3]][nu])
-    staple = np.dot(staple,  np.conjugate(link[xppmunu[0]][xppmunu[1]][xppmunu[2]][xppmunu[3]][mu]))
     staple = np.dot(staple,  np.conjugate(link[xpmunu[0]][xpmunu[1]][xpmunu[2]][xpmunu[3]][mu]))
+    staple = np.dot(staple,  np.conjugate(link[xpnu[0]][xpnu[1]][xpnu[2]][xpnu[3]][mu]))
     #g.printing_matrix(staple)
-    staple = np.dot(staple, np.conjugate(link[xpnu[0]][xpnu[1]][xpnu[2]][xpnu[3]][nu]))
+    staple = np.dot(staple, np.conjugate(link[x[0]][x[1]][x[2]][x[3]][nu]))
     return np.absolute((1/3)*np.real(np.trace(staple)))
-    #g.printing_matrix(np.dot(staple,np.transpose(np.conjugate(staple))))
-
-def plaquette_specular(mu,nu, x,link):
-    staple = link[x[0]][x[1]][x[2]][x[3]][mu]
-    #g.printing_matrix(staple)
-    xpmu = moveup(x,mu)
-    xmmu = movedown(x,mu)
-    xpnu = moveup(x,nu)
-    xmnu = movedown(x,nu)
-    xpmunu = moveup(xpnu,mu)
-    xpmumnu = movedown(moveup(xpnu,mu),nu)
-    staple = np.dot(staple,  np.conjugate(link[xpmu[0]][xpmu[1]][xpmu[2]][xpmu[3]][nu]))
-    staple = np.dot(staple,  np.conjugate(link[xpmumnu[0]][xpmumnu[1]][xpmumnu[2]][xpmumnu[3]][mu]))
-    #g.printing_matrix(staple)
-    staple = np.dot(staple, link[xmnu[0]][xmnu[1]][xmnu[2]][xmnu[3]][nu])
-    return np.absolute((1/3)*np.real(np.trace(staple)))
-    #g.printing_matrix(np.dot(staple,np.transpose(np.conjugate(staple))))
     
-def S(nu,x,link):  ###works better 0.48 and acceptance rate 0.45/0.50
+
+def S(nu,x,link):  ###works better 0.48 and acceptance rate 0.45/0.50 don't touch
     a = 0
     for deltanu in range(0,3-nu):
         deltanu = deltanu + 1
@@ -106,24 +142,11 @@ def S(nu,x,link):  ###works better 0.48 and acceptance rate 0.45/0.50
         a = a + plaquette(nu,nu-deltanu,x,link)
     return -beta*a
 
-def S_p(nu,x,link):  ###works 0.51 axa and 0.267 ax2a
-    a = 0
-    for deltanu in range(0,3-nu):
-        deltanu = deltanu + 1
-        a = a + plaquette(nu + deltanu,nu,x,link) + plaquette(nu + deltanu,nu,moveup(x,nu + deltanu),link) + plaquette(nu+deltanu,nu, movedown(x,nu+deltanu),link)
-    for deltanu in range(0,nu):
-        deltanu = deltanu+1
-        a = a + plaquette(nu,nu-deltanu,x,link)+ plaquette(nu,nu-deltanu,moveup(moveup(x,nu-deltanu),nu),link)+ plaquette(nu,nu-deltanu,movedown(moveup(x,nu-deltanu),nu),link)+  plaquette(nu,nu-deltanu,moveup(movedown(x,nu-deltanu),nu),link)+ plaquette(nu,nu-deltanu,movedown(movedown(x,nu-deltanu),nu),link)
-    return -beta*a
 
 
-def S_new(mu,x,link):
-    a = 0
-    for dperp in range(0,3):
-        if (dperp < mu):
-           a =  a + plaquette(mu,dperp,x,link) + plaquette_specular(mu,dperp,x,link)
-    return beta*a 
 
+
+    
 R = g.random_matrices()
 #bubblesort_autojit = numba.jit(bubblesort)
 
@@ -138,11 +161,11 @@ def update(link):     #### change rate 0.4 with eps = 0.24
                     for d in range(0,4):
                         oldlink[i][j][k][l][d] = link[i][j][k][l][d]
                         x = [i,j,k,l]
-                        old_S = S(d,x,link)
+                        old_S = S_new_improved(d,x,link)
                         I = (int(r.uniform(0,200)))
                        # R = g.random_matrices()[I]
                         link[i][j][k][l][d] = np.dot(R[I],link[i][j][k][l][d])
-                        dS = S(d,x,link)-old_S
+                        dS = S_new_improved(d,x,link)-old_S
                         tot = tot + 1
                         if dS>0 and math.exp(-dS)<r.uniform(0,1):
                             link[i][j][k][l][d] = oldlink[i][j][k][l][d]
@@ -159,9 +182,9 @@ def compute(link):
                     for mu  in range(0,4):
                         for dperp in range(0,3):
                             if (dperp != mu):
-                                loop =  loop + wilson_loop_2aa(dperp,mu,x,link)
+                                loop =  loop + wilson_loop_a2a_new(mu,dperp,x,link)
     return loop/(N*N*N*N*12)
-x = [0,0,3,1]
+
 def MCaverage(link):
     plaquette_avg = [0]*N_cf
     for i in range(0,50): ###thermalization
@@ -173,7 +196,7 @@ def MCaverage(link):
         for j in range(0,N_cor):
             update(link)
            
-        plaquette_avg[alpha] = wilson_loop_2aa(1,2,x,link)
+        plaquette_avg[alpha] = compute(link)
     return plaquette_avg
 
 
